0b3829b80773dbba225952407893374c6fc1681e
karthik <karthik.software@protonmail.com>
2025-04-04 16:06:41 +0530

    edited server.js

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express')
-<<<<<<< HEAD
 const app = express()
 const cors = require('cors')
 const PORT = 5000
 
 app.use(cors())
-=======
-
-const app = express()
-const PORT = 5000
->>>>>>> e7087d88d2e75fc0135b25de75416e291926633d
 
 const data = {
     'python' : {
@@ -32,6 +26,10 @@ const data = {
         'First appeared' : '4 December 1995',
         'Designed by' : 'Brendan Eich of Netscape initially'
     },
+    'js' : {
+        'First appeared' : '4 December 1995',
+        'Designed by' : 'Brendan Eich of Netscape initially'
+    },
     'php' : {
         'First appeared' : '8 June 1995',
         'Developer' : 'The PHP Development Team'
@@ -40,17 +38,6 @@ const data = {
         'First appeared' : 'unknown',
         'Developer' : 'unknown'
     }
-<<<<<<< HEAD
-=======
-    
-// Classic Visual Basic
-// MATLAB
-// Swift
-// Delphi/Object Pascal
-// Ruby
-// Perl
-// Objective-C
->>>>>>> e7087d88d2e75fc0135b25de75416e291926633d
 }
 
 app.get('/', (req,res) => {
